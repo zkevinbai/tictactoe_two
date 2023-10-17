dd143c79763f14e1046595185d3b95ddbbbcfa80
Kevin Bai <lkevin957@gmail.com>
2023-10-17 12:11:41 -0700

    Squares

--- src/App.js
+++ src/App.js
@@ -1,3 +1,27 @@
-export default function Square() {
-  return <button className="square">X</button>;
+function Square() {
+  return (
+    <button className="square">1</button>
+  )
+}
+
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  )
 }
