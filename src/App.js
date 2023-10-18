05701a169f93885fd482fee676c47c0730b640bc
Kevin Bai <lkevin957@gmail.com>
2023-10-18 14:45:08 -0700

    render row

--- src/App.js
+++ src/App.js
@@ -28,7 +28,6 @@ export function Board({
   // const [squares, setSquares] = useState(Array(9).fill(null))
 
   function handleClick (idx) {
-    debugger
     console.log(squares, calculateWinner(squares))
 
     if (calculateWinner(squares) || squares[idx]) { 
@@ -46,6 +45,25 @@ export function Board({
     onPlay(nextSquares)
   }
 
+  const renderRow = (row) => {
+    return (
+      <div className='board-row' key={row}>
+        {
+          Array(3).fill(null).map((_ , col) => {
+            const squareIndex = row * 3 + col
+            return (
+              <Square
+                key={squareIndex}
+                value={squares[squareIndex]}
+                onSquareClick={() => handleClick(squareIndex)}
+              />
+            )
+          })
+        }
+      </div>
+    )
+  }
+
   const winner = calculateWinner(squares)
   let status
   if (winner) {
@@ -57,20 +75,11 @@ export function Board({
   return (
     <>
       <div className='status'>{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+
+      <div className='board'>
+        {
+          Array(3).fill(null).map((_,row) => renderRow(row))
+        }
       </div>
 
       <div className='reset'>
