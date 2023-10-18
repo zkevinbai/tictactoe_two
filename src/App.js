3d5655cfc823756257b356eed7d4331821091f3f
Kevin Bai <lkevin957@gmail.com>
2023-10-18 11:49:48 -0700

    add core time travel logic

--- src/App.js
+++ src/App.js
@@ -83,22 +83,26 @@ export function Board({
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true)
   const [history, setHistory] = useState([Array(9).fill(null)])
-
-  const currentSquares = history[history.length - 1]
+  const [currentMove, setCurrentMove] = useState(0)
+  const currentSquares = history[currentMove]
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares])
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
+    setHistory(nextHistory)
+    setCurrentMove(nextHistory.length - 1)
     setXIsNext(!xIsNext)
   }
 
   function handleReset() {
-    debugger
     setHistory([Array(9).fill(null)])
+    setCurrentMove(0)
   }
 
   // console.log({history, currentSquares})
 
   function jumpTo(nextMove) {
+    setCurrentMove(nextMove)
+    setXIsNext(currentMove % 2 === 0)
   }
 
   const moves = history.map((squares, move) => {
